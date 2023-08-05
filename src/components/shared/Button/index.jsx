5d67b87dfe1b61f98b3b4d8b6350b19cc2a5b467
Burak Haşıcı <burakhasici@gmail.com>
2023-08-05 22:54:24 +0300

    Button text boldened

--- src/components/shared/Button/index.jsx
+++ src/components/shared/Button/index.jsx
@@ -4,7 +4,7 @@ export const PrimaryButton = ({ children, onClick, className, style }) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-secondary text-primary text-[1.125rem] px-[1.563rem] py-[0.906rem] ${className} border-2 border-[transparent] hover:border-2 hover:border-secondary hover:text-secondary hover:bg-[transparent] min-w-[140px] transition-all `}
+      className={`bg-secondary text-primary font-bold text-[1.125rem] px-[1.563rem] py-[0.906rem] ${className} border-2 border-[transparent] hover:border-2 hover:border-secondary hover:text-secondary hover:bg-[transparent] min-w-[140px] transition-all `}
       style={style}
     >
       {children}
@@ -16,7 +16,7 @@ export const SecondaryButton = ({ children, onClick, className, style }) => {
   return (
     <button
       onClick={onClick}
-      className={`text-ghostwhite border-b-[0.188rem] border-b-secondary transition-all hover:text-secondary ${className}`}
+      className={`text-ghostwhite font-bold border-b-[0.188rem] border-b-secondary transition-all hover:text-secondary ${className}`}
       style={style}
     >
       {children}
